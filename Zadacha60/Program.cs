e9c67744987fe656da2f362759371ab1132d0c2d
woynaw <woynaw@yandex.ru>
2023-06-07 16:19:32 +0300

    add Zadacha60

--- Zadacha60/Program.cs
+++ Zadacha60/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+void Print3DArray(int[,,] arg)
+{
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        for (int j = 0; j < arg.GetLength(1); j++)
+        {
+            for (int k = 0; k < arg.GetLength(2); k++)
+            {
+                Console.Write($"({i},{j},{k}) ");
+                Console.Write($"{arg[i, j, k]} ");
+            }
+
+        }
+        Console.WriteLine();
+    }
+}
+
+void Random3DArrayNoDuplicates(int[,,] arg)
+{
+    int[] array = new int[8];
+    int rand;
+    int pos = 0;
+    for (int i = 0; i < temp.GetLength(0); i++)
+    {
+        array[i] = new Random().Next(10, 99 + 1);
+        rand = array[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (array[i] == array[j])
+                {
+                    array[i] = new Random().Next(10, 99 + 1);
+                    j = 0;
+                    rand = array[i];
+                }
+                rand = array[i];
+            }
+        }
+    }
+
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        for (int j = 0; j < arg.GetLength(1); j++)
+        {
+            for (int k = 0; k < arg.GetLength(2); k++)
+            {
+                arg[i, j, k] = array[pos];
+                pos++;
+            }
+        }
+    }
+}
+
+
+int[,,] array = new int[2, 2, 2];
+Random3DArrayNoDuplicates(array);
+Print3DArray(array);
