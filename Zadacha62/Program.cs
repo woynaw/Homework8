1b00beee823b0c97a94989c2331ddd1fa119c0c2
woynaw <woynaw@yandex.ru>
2023-06-07 16:49:54 +0300

    add Zadacha62

--- Zadacha62/Program.cs
+++ Zadacha62/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Spiral(int[,] arg)
+{
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= 16)
+{
+
+    arg[i, j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < arg.GetLength(1) - 1) // условия движения вправо
+        j++;
+    else if (i < j && i + j >= arg.GetLength(0) - 1) // условия движения вниз
+        i++;
+    else if (i >= j && i + j > arg.GetLength(1) - 1) // условия движения влево
+        j--;
+    else  // условия движения вверх
+        i--;
+}
+}
+
+void Print2DArray(int[,] arg)
+{
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        for (int j = 0; j < arg.GetLength(1); j++)
+        {
+            Console.Write($" {arg[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] array = new int[4,4];
+Spiral(array);
+Print2DArray(array);
